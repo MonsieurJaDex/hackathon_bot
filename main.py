a76213a38d35d21f881a5bb14d7d90cde8731532
MonsieurJaDex <coder.dinya@gmail.com>
2026-01-17 02:16:57 +0300

    main.py: Added comments and logging level set

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import asyncio
+import logging
 
 from aiogram import Dispatcher, Bot, types
 from aiogram.filters import CommandStart
 
 from config import AppConfig
 
+# initializing bot dispatcher object
 dp = Dispatcher()
 
 
+# basic test echo handler
 @dp.message(CommandStart())
 async def echo(message: types.Message):
     await message.answer(
@@ -15,12 +18,19 @@ async def echo(message: types.Message):
     )
 
 
+# main application function
 async def main():
+    # main configuration object initialization
     config = AppConfig()
 
+    # set level of logging (to be changed)
+    logging.basicConfig(level=logging.INFO)
+
+    # initialize bot instance and start polling
     bot = Bot(token=config.bot_token)
     await dp.start_polling(bot)
 
 
+# run main function in case program start point is main.py file
 if __name__ == "__main__":
     asyncio.run(main())
