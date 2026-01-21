d43c2f8e72442e5168e8c8f6cbd9c16452b4ec4a
MonsieurJaDex <coder.dinya@gmail.com>
2026-01-21 05:33:29 +0300

    Added more comments to service.py

--- app/database/service.py
+++ app/database/service.py
@@ -7,28 +7,34 @@ from app.database.schemas import MediaDTO
 from app.misc import Singleton
 
 
+# service that union database and user levels of app logic
 class MediaService(Singleton):
+    # get factory for session initialization
     factory: async_sessionmaker[AsyncSession]
 
     def __init__(self, factory: async_sessionmaker[AsyncSession] = session_factory):
         self.factory = factory
 
+    # media insertion method
     async def insert_media(self, dto: MediaDTO) -> int:
         async with self.factory() as session:
             return await CRUDMedia.create(session, dto)
 
+    # finding media by id method
     async def get_media_by_id(self, media_id: int) -> MediaContent | None:
         async with self.factory() as session:
             return await CRUDMedia.find_by_id(session, media_id)
 
+    # get all exists media
     async def get_all_media(self) -> list[MediaContent]:
         async with self.factory() as session:
             return await CRUDMedia.find_all(session)
 
+    # get latest N media by day
     async def get_latest_n_media(self, n: int) -> list[MediaContent]:
         async with self.factory() as session:
             return await CRUDMedia.find_n_media(session, n)
 
 
-# init singleton MediaService
+# initialize singleton MediaService
 MediaService()
